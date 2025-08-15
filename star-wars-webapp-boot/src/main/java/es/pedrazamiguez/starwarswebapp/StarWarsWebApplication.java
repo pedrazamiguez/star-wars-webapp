de0c2d0a3650e1c70ad14e291056a12f86e8444b
Andrés Pedraza Míguez <pedraza.miguez@gmail.com>
2025-08-15 14:01:07 +0200

    Created multi module project

---
+++ star-wars-webapp-boot/src/main/java/es/pedrazamiguez/starwarswebapp/StarWarsWebApplication.java
@@ -0,0 +1,13 @@
+package es.pedrazamiguez.starwarswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsWebApplication.class, args);
+	}
+
+}
