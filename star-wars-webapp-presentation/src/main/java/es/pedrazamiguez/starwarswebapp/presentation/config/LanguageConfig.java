aa95ef8253cad7bab0c7abbbb0fa8155548b7990
Andrés Pedraza Míguez <pedraza.miguez@gmail.com>
2025-08-15 16:28:55 +0200

    Implemented structured thymeleaf templating system integration

---
+++ star-wars-webapp-presentation/src/main/java/es/pedrazamiguez/starwarswebapp/presentation/config/LanguageConfig.java
@@ -0,0 +1,32 @@
+package es.pedrazamiguez.starwarswebapp.presentation.config;
+
+import java.util.Locale;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class LanguageConfig implements WebMvcConfigurer {
+
+  @Bean
+  public SessionLocaleResolver localeResolver() {
+    final SessionLocaleResolver resolver = new SessionLocaleResolver();
+    resolver.setDefaultLocale(Locale.forLanguageTag("en"));
+    return resolver;
+  }
+
+  @Bean
+  public LocaleChangeInterceptor localeChangeInterceptor() {
+    final LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+    interceptor.setParamName("language");
+    return interceptor;
+  }
+
+  @Override
+  public void addInterceptors(final InterceptorRegistry registry) {
+    registry.addInterceptor(this.localeChangeInterceptor());
+  }
+}
