9c717bd1caaab7920631ff8f6753f854f0fdd465
Andrés Pedraza Míguez <pedraza.miguez@gmail.com>
2025-08-16 13:50:27 +0200

    Applied formatting

--- star-wars-webapp-boot/src/main/java/es/pedrazamiguez/starwarswebapp/StarWarsWebApplication.java
+++ star-wars-webapp-boot/src/main/java/es/pedrazamiguez/starwarswebapp/StarWarsWebApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StarWarsWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StarWarsWebApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(StarWarsWebApplication.class, args);
+  }
 }
--- star-wars-webapp-infrastructure/star-wars-webapp-api-client/src/main/java/es/pedrazamiguez/starwarswebapp/apiclient/dto/PeopleResponseDto.java
+++ star-wars-webapp-infrastructure/star-wars-webapp-api-client/src/main/java/es/pedrazamiguez/starwarswebapp/apiclient/dto/PeopleResponseDto.java
@@ -1,8 +1,7 @@
 package es.pedrazamiguez.starwarswebapp.apiclient.dto;
 
-import lombok.Data;
-
 import java.util.List;
+import lombok.Data;
 
 @Data
 public class PeopleResponseDto {
